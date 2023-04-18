a28aa91469c8344a5978f914d9b23c42f0c594de
Kamonchanok <ka.kamonchanokk@gmail.com>
2023-04-18 18:04:51 +0700

    initial Backend

---
+++ ManagerInternship-backend/src/main/java/demo/backend/ManageInternship/model/payload/test.java
@@ -0,0 +1,4 @@
+package demo.backend.ManageInternship.model.payload;
+
+public class test {
+}
---
+++ ManagerInternship-backend/src/test/java/demo/backend/ManageInternship/ManageInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.backend.ManageInternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
